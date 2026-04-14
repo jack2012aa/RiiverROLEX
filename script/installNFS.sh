12b27188208c3b741d2b9c9429fd47b3485d0326
ChangYu Huang <jack.mainwork@gmail.com>
2026-04-14 15:55:22 -0400

    Idempotency check

--- script/installNFS.sh
+++ script/installNFS.sh
@@ -9,8 +9,13 @@ sudo chown -R $USER:$USER /nfs_share
 echo "rpcbind: 10.10.1.0/24" | sudo tee -a /etc/hosts.allow
 sudo sed -i 's/-h 127.0.0.1//g' /etc/default/rpcbind
 
-echo "/nfs_share 10.10.1.0/24(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+EXPORT_LINE="/nfs_share 10.10.1.0/24(rw,sync,no_root_squash,no_subtree_check)"
+
+if ! grep -qF "$EXPORT_LINE" /etc/exports; then
+    echo "$EXPORT_LINE" | sudo tee -a /etc/exports
+fi
 exportfs -a
 systemctl restart nfs-kernel-server
 
-
+mkdir -p /nfs_share/workloads/uniform
+mkdir -p /nfs_share/workloads/zipfian
--- script/mountNFS.sh
+++ script/mountNFS.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 apt update && apt install nfs-common
 mkdir -p /nfs_share
-echo "10.10.1.1:/nfs_share  /nfs_share  nfs  defaults  0  0" | sudo tee -a /etc/fstab
+
+MOUNT="10.10.1.1:/nfs_share  /nfs_share  nfs  defaults  0  0" 
+if ! grep -qF $MOUNT /etc/exports; then
+    echo $MOUNT | sudo tee -a /etc/fstab
+fi
